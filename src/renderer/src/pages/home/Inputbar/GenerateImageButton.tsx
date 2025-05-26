70d2c4db1f8532a4620e7813ff61c321c20892d6
purefkh <purefkh@gmail.com>
2025-05-26 21:26:54 +0800

    feat: support to show ThinkButton and GenerateImageButton with metion models

--- src/renderer/src/pages/home/Inputbar/GenerateImageButton.tsx
+++ src/renderer/src/pages/home/Inputbar/GenerateImageButton.tsx
@@ -8,21 +8,22 @@ import { useTranslation } from 'react-i18next'
 interface Props {
   assistant: Assistant
   model: Model
+  mentionModels: Model[]
   ToolbarButton: any
   onEnableGenerateImage: () => void
 }
 
-const GenerateImageButton: FC<Props> = ({ model, ToolbarButton, assistant, onEnableGenerateImage }) => {
+const GenerateImageButton: FC<Props> = ({ model, mentionModels, ToolbarButton, assistant, onEnableGenerateImage }) => {
   const { t } = useTranslation()
+
+  const supportGenerateImage = isGenerateImageModel(model) || mentionModels.some(isGenerateImageModel)
 
   return (
     <Tooltip
       placement="top"
-      title={
-        isGenerateImageModel(model) ? t('chat.input.generate_image') : t('chat.input.generate_image_not_supported')
-      }
+      title={supportGenerateImage ? t('chat.input.generate_image') : t('chat.input.generate_image_not_supported')}
       arrow>
-      <ToolbarButton type="text" disabled={!isGenerateImageModel(model)} onClick={onEnableGenerateImage}>
+      <ToolbarButton type="text" disabled={!supportGenerateImage} onClick={onEnableGenerateImage}>
         <Image size={18} color={assistant.enableGenerateImage ? 'var(--color-link)' : 'var(--color-icon)'} />
       </ToolbarButton>
     </Tooltip>
